5b9f1485facaf514103ef0108c4ce6c6125d940f
DogeisCut <rickisparks06@gmail.com>
2025-07-22 21:34:10 -0600

    Cutscene manager now has a signal for when a new cutscene starts.

--- cutscenea/cutscene_manager.gd
+++ cutscenea/cutscene_manager.gd
@@ -2,8 +2,11 @@ extends Node
 
 var active_cutscene_instances: Array[CutsceneInstance]
 
+signal cutscene_instance_created(cutscene_instance: CutsceneInstance)
+
 func play(caller: Node, cutscene: Cutscene) -> CutsceneInstance:
 	var instance: CutsceneInstance = CutsceneInstance.new(caller, cutscene)
+	cutscene_instance_created.emit(instance)
 	active_cutscene_instances.append(instance)
 	instance.finished.connect(
 		func():
